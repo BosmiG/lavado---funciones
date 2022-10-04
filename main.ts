615984c8a1f220f8153508c5436ac7877d8e7fec
BosmiG <113538823+BosmiG@users.noreply.github.com>
2022-10-04 15:53:58 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,426 @@
+function CicloB () {
+    for (let index = 0; index < 3; index++) {
+        EntraAgua()
+    }
+    for (let index = 0; index < 3; index++) {
+        Jabonar()
+    }
+    for (let index = 0; index < 3; index++) {
+        Centrifugado()
+    }
+    for (let index = 0; index < 3; index++) {
+        SaleAgua()
+    }
+    for (let index = 0; index < 3; index++) {
+        Secado()
+    }
+}
+function EntraAgua () {
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+}
+function Secado () {
+    basic.showLeds(`
+        . # . . .
+        # . # . .
+        . . . # .
+        . . . . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . # . #
+        . # . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . # . #
+        . . . # .
+        . . . . .
+        `)
+}
+function Centrifugado () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # #
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # #
+        . . # . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # #
+        . . # . #
+        . . . . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # #
+        . . # . #
+        . . . . #
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # #
+        . . # . #
+        . . . . #
+        . . . # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # #
+        . . # . #
+        . . . . #
+        . . # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # #
+        . . # . #
+        . . . . #
+        . # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # #
+        . . # . #
+        . . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # #
+        . . # . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . # # #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # . . . .
+        # . # # #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # . . .
+        # . # # #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # # . .
+        # . # # #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # # # .
+        # . # # #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . # # #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
+}
+function SaleAgua () {
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+function CicloA () {
+    EntraAgua()
+    Centrifugado()
+    SaleAgua()
+    Secado()
+}
+function Jabonar () {
+    basic.showLeds(`
+        . . . # .
+        . . # . #
+        . . . # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . # . #
+        . . . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . # .
+        . . # . #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . # .
+        . . # . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . # . . .
+        # . # . .
+        . # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        # . # . .
+        . # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . . .
+        # . # . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # . . .
+        # . # . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . # . . .
+        `)
+}
+basic.forever(function () {
+    while (input.buttonIsPressed(Button.A)) {
+        CicloA()
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    while (input.buttonIsPressed(Button.B)) {
+        CicloB()
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
